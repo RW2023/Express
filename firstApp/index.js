75ae0935e654c6782628115d0b390380ca6c7beb
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-11 09:54:10 -0400

    setup response method

--- firstApp/index.js
+++ firstApp/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app =express();
-console.dir(app);
+
+app.use((req, res)=>{
+    console.log("WE GOT A REQUEST!")
+    res.send("Yow We got your request!!")
+})
 
 app.listen(3000, ()=>{
     console.log('LISTENING ON PORT 3000!')
