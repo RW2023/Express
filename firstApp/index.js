bcd887fdeeebde0d171d9d880f0a5caac05c9aa7
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-04-12 06:16:48 -0400

    change response

--- firstApp/index.js
+++ firstApp/index.js
@@ -3,7 +3,7 @@ const app =express();
 
 app.use((req, res)=>{
     console.log("WE GOT A REQUEST!")
-    res.send("Yow We got your request!!")
+    res.send('<h1>This is My Web Page</h1>')
 })
 
 app.listen(3000, ()=>{
